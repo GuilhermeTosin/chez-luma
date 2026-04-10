909c5db3a104c17df024ffa07c2a515dec30a68d
GuilhermeTosin <contact@guilhermetosin.com>
2026-04-10 17:49:27 -0400

    fix: setup base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// "homepage": "https://chezluma.com/",
 export default defineConfig({
   plugins: [react()],
-  base: '/chez-luma/',
+   base: '/',
 })
